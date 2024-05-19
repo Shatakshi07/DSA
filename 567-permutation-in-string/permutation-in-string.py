e6a1eb292b66afbfbf8397bf9aa0cb2f27a5e3f3
Shatakshi07 <53287169+Shatakshi07@users.noreply.github.com>
2024-05-19 15:25:21 +0530

    Time: 155 ms (27.24%) | Memory: 16.7 MB (32.27%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,5 +1,5 @@
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
+    def checkInclusion2(self, s1: str, s2: str) -> bool:
         
         if len(s1) > len(s2):
             return False
@@ -35,6 +35,20 @@ class Solution:
             l += 1
 
         return matches == 26
+
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        cntr, w = Counter(s1), len(s1)   
+
+        for i in range(len(s2)):
+            if s2[i] in cntr: 
+                cntr[s2[i]] -= 1
+            if i >= w and s2[i-w] in cntr: 
+                cntr[s2[i-w]] += 1
+
+            if all([cntr[i] == 0 for i in cntr]): # see optimized code below
+                return True
+
+        return False
 
 
     
