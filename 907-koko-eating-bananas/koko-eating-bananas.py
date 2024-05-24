8ba2a9d2116db1ac041b74ec0574620843c26152
Shatakshi07 <53287169+Shatakshi07@users.noreply.github.com>
2024-05-24 23:06:12 +0530

    Time: 259 ms (75.89%) | Memory: 18 MB (91.89%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+    def minEatingSpeed2(self, piles: List[int], h: int) -> int:
         l, r = 1, max(piles)
         res = r
 
@@ -15,3 +15,20 @@ class Solution:
             else:
                 l = k + 1
         return res
+
+        
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = ceil(sum(piles) / h) # lower bound of Binary Search 
+        right = max(piles) # upper bound of Binary Search 
+        while left < right:
+            mid = (left + right) // 2 # we check for k=mid
+            total_time = 0
+            for i in piles:
+                total_time += ceil(i / mid)
+                if total_time > h:
+                    break
+            if total_time <= h:
+                right = mid # answer must lie to the left half (inclusive of current value ie mid)
+            else:
+                left = mid + 1 # answer must lie to the right half (exclusive of current value ie mid)
+        return right
